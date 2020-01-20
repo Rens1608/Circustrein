6c0e90d96b8b01eb773dd5734044350ec53a8551
rens.vanmelis1234@gmail.com <rens.vanmelis1234@gmail.com>
2020-01-20 20:12:19 +0100

    Unit tests gemaakt

--- CircustreinTest/CircustreinTest.cs
+++ CircustreinTest/CircustreinTest.cs
@@ -9,33 +9,47 @@ namespace CircustreinTest
     [TestClass]
     public class CircustreinTest
     {
-        private Wagon wagon;
-        [TestInitialize]
-        public void Setup()
-        {
-            wagon = new Wagon();
-        }
-
-        [TestMethod]
-        public void Empty_List_Should_Return_False()
-        {
-            //arrange
-            List<Animal> input = new List<Animal>();
-            //act
-            bool output = wagon.LookIfAnimalFits(input);
-            //assert
-            Assert.AreEqual(false, output);
-        }
-
         [TestMethod]
         public void One_Animal_In_List_Return_True()
         {
             //arrange
+            Wagon wagon = new Wagon();
             List<Animal> input = new List<Animal>() { new Animal(3, Animal.Race.Carnivore) };
             //act
             bool output = wagon.LookIfAnimalFits(input);
             //assert
             Assert.AreEqual(true, output);
+        }
+
+        [TestMethod]
+        public void Place_Herbivore_With_Carnivore_In_Wagon_Succeeds()
+        {
+            Wagon wagon = new Wagon();
+            List<Animal> input = new List<Animal>() { new Animal(3, Animal.Race.Herbivore) };
+            wagon.wagonAnimals.Add(new Animal(1, Animal.Race.Carnivore));
+            bool output = wagon.LookIfAnimalFits(input);
+            Assert.AreEqual(true, output);
+        }
+
+        [TestMethod]
+        public void Place_Herbivore_With_Carnivore_In_Wagon_Fails()
+        {
+            Wagon wagon = new Wagon();
+            List<Animal> input = new List<Animal>() { new Animal(3, Animal.Race.Herbivore) };
+            wagon.wagonAnimals.Add(new Animal(3, Animal.Race.Carnivore));
+            bool output = wagon.LookIfAnimalFits(input);
+            Assert.AreEqual(false, output);
+        }
+
+        [TestMethod]
+        public void Place_Herbivore_With_Animals_In_Wagon_Fails()
+        {
+            Wagon wagon = new Wagon();
+            List<Animal> input = new List<Animal>() { new Animal(5, Animal.Race.Herbivore) };
+            wagon.wagonAnimals.Add(new Animal(3, Animal.Race.Carnivore));
+            wagon.wagonAnimals.Add(new Animal(5, Animal.Race.Herbivore));
+            bool output = wagon.LookIfAnimalFits(input);
+            Assert.AreEqual(false, output);
         }
     }
 }
